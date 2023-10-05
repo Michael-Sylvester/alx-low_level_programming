9291658742f5c1ba1096b085a67beb43152e68ea
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-05 10:15:45 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - fuck you betty. im printing a string
+ * No parameter
+ *
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: return nothing
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
