4d30871f4115d647c9ddc00ca45eaa3c48a47f84
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-05 10:05:36 -0700

    betty fucking sucks. Update task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Short description, single line
+ * No parameter
+ *
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
 int main(void)
 {
- /* This program prints a string*/
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
