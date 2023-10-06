979f1064f7bf1a7fc349a49651eb857a94d356c3
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-06 10:34:37 -0700

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 /**
- * main - blah blah
+ * main - print small alphabets in reverse
  * no parameters
  *
  * Return: nothing
  */
+int main(void)
+{
+  /*ASCII values o small alphabets starts from 97~122*/
+int alpha = 122;
+
+while (alpha > 96)
+{
+putchar(alpha);
+alpha--;
+}
+putchar('\n');
+return (0);
+}
