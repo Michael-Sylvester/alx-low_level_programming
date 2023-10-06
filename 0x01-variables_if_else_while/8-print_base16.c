aa5d3ad900dd1e1ecb1a0bf55dee0ea6777b1848
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-06 11:00:50 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,3 +5,23 @@
  *
  * Return: nothing
  */
+int main(void)
+{
+  /*Print numbers 0~9 then letters a~f.t hose are the numbers of base 16*/
+  /*ASCII numbers start from 48~57 and letters a to f are 97~102*/
+int base16 = 48;
+while (base16 < 58)
+{
+putchar(base16);
+base16++;
+}
+
+base16 = 97;
+while (base16 < 103)
+{
+putchar(base16);
+base16++;
+}
+putchar('\n');
+return (0);
+}
