ce9f346448aa1ffa11d43a13d41ee00698a82db9
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-06 10:22:42 -0700

    task 5 and 6

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,3 +5,15 @@
  *
  * Return: nothing
  */
+int main(void)
+{
+int num = 0;
+
+while (num < 10)
+{
+printf("%d", num);
+num++;
+}
+printf("\n");
+return (0);
+}
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,3 +5,14 @@
  *
  * Return: nothing
  */
+int main(void)
+{
+/* ASCII value of 0~9 is 48~57 */
+int num = 48;
+while (num < 58)
+{
+putchar(num);
+num++;
+}
+putchar('\n');
+}
