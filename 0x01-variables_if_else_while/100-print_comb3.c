09789d88dc004f2de891021ca764e4b69d11536c
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-09 06:47:59 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - blah blah
+ * no parameters
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+int i, j;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = i + 1; j <= 9; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+}
