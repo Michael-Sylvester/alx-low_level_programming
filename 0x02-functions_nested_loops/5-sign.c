268ef0e14e77642ce10fbf8ce510cb9391e7e272
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-10 17:48:05 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,26 @@
 #include "main.h"
 /**
- * function - blah blah
- * no parameters
+ * print_sign - blah blah
+ * @n : parameters
  *
  * Return: nothing
  */
-int function(void)
+int print_sign(int n)
 {
-
+if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else if (n == 0)
+{
+_putchar('0');
 return (0);
+}
+else
+{
+_putchar('+');
+return (1);
+}
+
 }
