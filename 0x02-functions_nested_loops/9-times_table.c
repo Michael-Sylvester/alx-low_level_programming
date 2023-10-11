5b0bda9293ec5907725fecd675da898eaae03ec7
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-10 19:32:04 -0700

    task 9 update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,14 +20,13 @@ if (column > 0)
 _putchar(',');
 _putchar(' ');
 
-
 if (product < 10)
 _putchar(' ');
 }
+
 _putchar((product / 10) + '0');
 _putchar((product % 10) + '0');
 }
 _putchar('\n');
 }
-
 }
