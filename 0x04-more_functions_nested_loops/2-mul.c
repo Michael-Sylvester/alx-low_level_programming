3350a2585e2115ae5177f3c862814c9f0826fbfd
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-12 06:54:07 -0700

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * function - blah blah
- * no parameters
- *
+ * mul - blah blah
+ * @a : parameters
+ * @b : paramter
  * Return: nothing
  */
-int function(void)
+int mul(int a, int b)
 {
 
-return (0);
+return (a * b);
 }
