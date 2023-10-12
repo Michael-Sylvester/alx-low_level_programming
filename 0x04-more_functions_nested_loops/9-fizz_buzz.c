80182cb5339ce45db148232b1cb1c1db47d07869
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-12 12:52:11 -0700

    task 9 update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,10 +17,13 @@ else if (num % 3 == 0)
 printf("Fizz ");
 else if (num % 5 == 0)
 printf("Buzz ");
+else if (num == 100)
+printf("%d", num);
 else
 printf("%d ", num);
 }
 putchar('\n');
 
+printf("%d ", num);
 return (0);
 }
