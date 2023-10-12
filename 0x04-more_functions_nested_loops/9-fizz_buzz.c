707f4a3df151118eae3f2cea17755e41cc1b7d1c
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-12 12:29:09 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * function - blah blah
+ * main - blah blah
  * no parameters
  *
  * Return: nothing
  */
-int function(void)
+void main(void)
 {
-
-return (0);
+for (int num = 1; num <= 100; num++)
+{
+if (num % 3 == 0 && num % 5 == 0)
+printf("FizzBuzz ");
+else if (num % 3 == 0)
+printf("Fizz ");
+else if (num % 5 == 0)
+printf("Buzz ");
+else if (num == 100)
+printf("%d", num);
+else
+printf("%d ", num);
+}
+putchar('\n');
 }
