c6f19ee89c929f540737949948491d42151a12b0
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-12 11:51:31 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,12 +1,30 @@
 #include "main.h"
 /**
- * function - blah blah
- * no parameters
+ * print_diagonal - blah blah
+ * @n : parameters
  *
  * Return: nothing
  */
-int function(void)
+void print_diagonal(int n)
 {
-
-return (0);
+if (n > 0)
+{
+int run = 1;
+while (run <= n)
+{
+int slash = 1;
+while (slash < run)
+{
+_putchar(' ');
+slash++;
+}
+_putchar('\\');
+_putchar('\n');
+run++;
+}
+}
+else
+{
+_putchar('\n');
+}
 }
