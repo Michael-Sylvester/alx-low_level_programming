c4a6c74c14703c9c7ed21bcd92067a8812326ded
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-16 14:52:44 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,12 +1,37 @@
 #include "main.h"
 /**
- *  - blah blah
- * no parameters
+ * puts_half - blah blah
+ * @str : parameters
  *
  * Return: nothing
  */
-void 
+void puts_half(char *str)
 {
+char temp = *str;
+int count = 0;
+int half = 0;
+int again = 0;
+while (*str != '\0')
+{
+count++;
+str++;
+}
 
-return (0);
+while (*str != temp)
+{
+str--;
+}
+
+half = count / 2;
+
+while (again != count)
+{
+if (again >= half)
+_putchar(*str);
+
+str++;
+again++;
+}
+
+_putchar('\n');
 }
