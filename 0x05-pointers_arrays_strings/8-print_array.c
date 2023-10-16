63d64f18b5bda827fd128f08c3082d5fb08c78d2
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-16 16:48:40 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,12 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 /**
- *  - blah blah
- * no parameters
- *
+ * print_array - blah blah
+ * @a : parameters
+ *@n :yo
  * Return: nothing
  */
-void 
+void print_array(int *a, int n)
 {
+int count = 0;
+while (count < n)
+{
+printf("%d", *a);
 
-return (0);
+if (count != n - 1)
+printf(", ");
+
+count++;
+a++;
+}
+_putchar('\n');
 }
