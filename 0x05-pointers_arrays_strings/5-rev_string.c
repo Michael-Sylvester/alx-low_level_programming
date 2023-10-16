40e41f5a40eb82855f7e53a0486e8ce1636a8b89
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-16 13:34:53 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,12 +1,38 @@
 #include "main.h"
 /**
- *  - blah blah
- * no parameters
+ * rev_string - blah blah
+ * @s : parameters
  *
  * Return: nothing
  */
-void 
+void rev_string(char *s)
 {
+char temp = *s;
+char rev[100];
+while (*s != '\0')
+{
+s++;
+}
+s--;
 
-return (0);
+
+int count = 0;
+while (*s != temp)
+{
+rev[count] = *s;
+s--;
+count++;
+}
+if (temp != '\0')
+rev[count] = temp;
+
+count = 0;
+while (*s != '\0')
+{
+*s = rev[count];
+s++;
+count++;
+}
+
+_putchar('\n');
 }
