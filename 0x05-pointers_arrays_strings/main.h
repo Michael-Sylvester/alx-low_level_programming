65f27b0c8a1eda56e2e674dd65f735fbdd7d910c
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-16 12:08:41 -0700

    main.h update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#defif MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -12,4 +12,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#fi MAIN_H
+#endif MAIN_H
