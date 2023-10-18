31881fa6a53b91613671442e233c43f33a24fd14
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-18 10:30:54 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,29 @@
 #include "main.h"
 /**
- *  - blah blah
- * no parameters
- *
+ * strncat - blah blah
+ * @dest : parameters
+ * @str : 2nd string
+ * @n : 
  * Return: nothing
  */
-void 
+char *_strncat(char *dest, char *src, int n)
 {
+  char *start = dest;
+  int size = 1;
+  while (*dest != '\0')
+    {
+      dest++;
+    }
 
+  while (*src != '\0' && size <= n)
+    {
+      *dest = *src;
+      dest++;
+      src++;
+      size++;
+    }
+ 
+  *dest = '\0';
 
+  return (start);
 }
