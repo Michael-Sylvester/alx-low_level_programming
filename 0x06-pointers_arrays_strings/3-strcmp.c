bdb57194b1a7caa8df55a61c3b3da0be8f636b5c
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-19 04:59:20 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,25 @@
 #include "main.h"
 /**
- *  - blah blah
- * no parameters
- *
+ * _strcmp - blah blah
+ * @s1 : parameters
+ * @s2 : 2nd string
  * Return: nothing
  */
-void 
+int _strcmp(char *s1, char *s2)
 {
+
+while (*s1 != '\0' && *s2 != '\0')
+{
+s1++;
+s2++;
+}
+
+if (*s1 == '\0' && *s2 == '\0')
+return (0);
+else if (*s1 == '\0')
+return (-1);
+else
+return (1);
 
 
 }
