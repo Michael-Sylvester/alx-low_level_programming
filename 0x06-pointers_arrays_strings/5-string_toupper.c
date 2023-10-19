421e0c7267be8844116bff91c65f78a9eb89ee55
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-19 09:07:18 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,31 @@
 #include "main.h"
 /**
- *  - blah blah
- * no parameters
+ * string_toupper - convert lowercase members of a string to upper
+ * @str : pointer to string
  *
  * Return: nothing
  */
-void 
+char *string_toupper(char *str)
 {
+int x = 0;
+char first  = str;
+char lower[] = "abcdefghijklmnopqrstuvwxyz";
+char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+while (*str != '\0')
+{
+x = 0;
+while(x < 26)
+{
+if (*str == lower[x])
+{
+*str = upper[x];
+break;
+}
+x++;
+}
+str++;
+}
+return (first);
 
 }
