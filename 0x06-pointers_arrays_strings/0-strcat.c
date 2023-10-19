7e09303ae6343cc0cc4d5f16018e0ba814ae0039
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-19 04:00:24 -0700

    readme

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,21 +7,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-  char *start = dest;
-  while (*dest != '\0')
-    {
-      dest++;
-    }
+char *start = dest;
+while (*dest != '\0')
+{
+dest++;
+}
 
-  while (*src != '\0')
-    {
-      *dest = *src;
-      dest++;
-      src++;
-    }
-  *dest = '\0';
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
 
-  return (start);
+return (start);
 }
 
 
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,24 @@
 #include "main.h"
 /**
- *  - blah blah
- * no parameters
- *
+ * _strncpy -copies one string into another using the pointers for both
+ * @dest : parameters
+ * @src : source string
+ * @n : Number of bytes to copy
  * Return: nothing
  */
-void 
+char *_strncpy(char *dest, char *src, int n)
 {
+  char *start = dest;
+  int size = 1;
+  while (*src != '\0' && size <= n)
+    {
+      *dest = *src;
+      dest++;
+      src++;
+    }
+  
+  *dest = '\0';
 
+  return (start);
 
 }
