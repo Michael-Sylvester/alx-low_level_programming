76c1a066161354602abe1202a81df4051b748357
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-19 07:11:01 -0700

    task 3 done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,14 +16,14 @@ return (*s1 - *s2);
 s1++;
 s2++;
 }
- return (*s1 - *s2);
+return (*s1 - *s2);
 /*
-if (*s1 == '\0' && *s2 == '\0')
-return (0);
-else if (*s1 == '\0')
-return (-1);
-else
-return (1);
+ *if (*s1 == '\0' && *s2 == '\0')
+ *return (0);
+ *else if (*s1 == '\0')
+ *return (-1);
+ *else
+ *return (1);
 */
 
 }
