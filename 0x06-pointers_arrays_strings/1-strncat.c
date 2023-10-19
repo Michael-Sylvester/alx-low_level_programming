8ab15cdee4acd46dd1cdd6a9a98a0482a2741a22
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-19 04:04:28 -0700

    task 1 betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,22 +8,22 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-  char *start = dest;
-  int size = 1;
-  while (*dest != '\0')
-    {
-      dest++;
-    }
+char *start = dest;
+int size = 1;
+while (*dest != '\0')
+{
+dest++;
+}
 
-  while (*src != '\0' && size <= n)
-    {
-      *dest = *src;
-      dest++;
-      src++;
-      size++;
-    }
- 
-  *dest = '\0';
+while (*src != '\0' && size <= n)
+{
+*dest = *src;
+dest++;
+src++;
+size++;
+}
 
-  return (start);
+*dest = '\0';
+
+return (start);
 }
