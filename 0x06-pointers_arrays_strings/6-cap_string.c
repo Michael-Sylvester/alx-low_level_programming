c537aa86f6a917c453151cb9dbf085bf9a503814
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-19 11:17:13 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,41 @@
 #include "main.h"
 /**
- *  - blah blah
- * no parameters
+ * cap_string - convert entire string to uppercase
+ * @str : parameters
  *
  * Return: nothing
  */
-void 
+char *cap_string(char *str)
+{
+int x = 0;
+int cap = 0;
+char *first  = str;
+char lower[] = "abcdefghijklmnopqrstuvwxyz";
+char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+while (*str != '\0')
+{
+x = 0;
+while (x < 26)
 {
 
+if (*str == lower[x] && cap == 1)
+{
+*str = upper[x];
+cap = 0;
+break;
+}
+x++;
+}
+
+if (*str == ',' || *str == ';' || *str == '.' || *str == '!' || *str == '?')
+cap = 1;
+
+if (*str == '"' || *str == '(' || *str == ')' || *str == '{' || *str == '}')
+cap = 1;
+
+str++;
+}
+return (first);
 
 }
