4993e17b9322687eea06e229a9cfb8fcf82bbd24
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-19 08:45:27 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,12 +1,33 @@
 #include "main.h"
 /**
- *  - blah blah
- * no parameters
- *
+ * reverse_array - blah blah
+ * @a : array
+ * @n : No of elements in array
  * Return: nothing
  */
-void 
+void reverse_array(int *a, int n)
 {
+int count = 0;
+int first = *a;
+int temp[1000];
+a = a + (n - 1);
 
+/*Copy all elements to the temp array in reverse  */
+while (*a != first)
+{
+temp[count] = *a;
+a--;
+count++;
+}
+temp[count] = first;
+
+/*input the reversed elements in temp into the destination array  */
+count = 0;
+while (count < n)
+{
+*a = temp[count];
+a++;
+count++;
+}
 
 }
