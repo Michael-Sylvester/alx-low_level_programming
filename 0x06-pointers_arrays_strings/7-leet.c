e2293d229715f7194a4b3673787262fdc8f49aa8
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-19 17:42:45 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,12 +1,32 @@
 #include "main.h"
 /**
- *  - blah blah
- * no parameters
+ * leet - convert tet to leet format
+ * @s : pointer for string
  *
  * Return: nothing
  */
-void 
+char *leet(char *s)
 {
+char *start = s;
+int i, j;
+char leet[10][2] = {
+{'a', '4'}, {'A', '4'}, {'e', '3'}, {'E', '3'},
+{'o', '0'}, {'O', '0'}, {'t', '7'}, {'T', '7'},
+{'l', '1'}, {'L', '1'}
+};
 
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (s[i] == leet[j][0])
+{
+s[i] = leet[j][1];
+break;
+}
+}
+}
+
+return (start);
 
 }
