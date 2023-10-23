c8c4b9d5056f23e472fd3e0e6698603c5a4d14ef
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-23 14:20:11 -0700

    task7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,12 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 /**
- *  - blah blah
- * no parameters
- *
+ * print_diagsums - sprint sum of the 2 diagonals of a 2d array
+ * @a : pointer to head of the array
+ * @size : number of elements of the array
  * Return: nothing
  */
-void 
+void print_diagsums(int *a, int size)
 {
+int col = 0;
+int row = 0;
+int revcol = size - 1;
+int sum1 = 0;
+int sum2 = 0;
 
-return ;
+while (row < size)
+{
+while (col < size)
+{
+sum1 += a[row * size + col];
+sum2 += a[row * size + revcol];
+
+row++;
+col++;
+revcol--;
+}
+}
+printf("%d, %d\n", sum1, sum2);
 }
