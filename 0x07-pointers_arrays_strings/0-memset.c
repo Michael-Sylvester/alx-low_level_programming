315b74117442966c29bf1f6fab1b8cd2569d1321
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-23 08:25:30 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,19 @@
 #include "main.h"
 /**
- *  - blah blah
- * no parameters
- *
- * Return: nothing
+ * _memset - fill n bytes of the array s points to with the value b
+ * @s :destination aray pointer
+ * @b : value to place in S
+ * @n : No of bytes of s that need to be filled with n
+ * Return: s
  */
-void 
+char *_memset(char *s, char b, unsigned int n)
 {
+int i = 0;
 
-return ;
+while (i < n - 1)
+{
+s[i] = b;
+}
+
+return (s);
 }
