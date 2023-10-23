8b0b7d90e3cc70a93827f1a98d86fe63ef2ea55b
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-23 09:11:15 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,20 @@
 #include "main.h"
+#include <stddef.h>
 /**
- *  - blah blah
- * no parameters
- *
- * Return: nothing
+ * _strchr - return 1st occurance of the charater c in the string s
+ * @s : the string being searched
+ * @c : the character being searched for
+ * Return: pointer location to c
  */
-void 
+char *_strchr(char *s, char c)
 {
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
 
-return ;
+s++;
+}
+
+return (NULL);
 }
