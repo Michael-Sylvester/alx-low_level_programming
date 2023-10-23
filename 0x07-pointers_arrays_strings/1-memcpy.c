a5d49cc6c94af1391d68fddc866db10ebf4c8243
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-23 09:00:53 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,23 @@
 #include "main.h"
 /**
- *  - blah blah
- * no parameters
- *
- * Return: nothing
+ * _memcpy - copy the first n contents of src into dest
+ * @dest : destination array
+ * @src : the source array
+ * @n : the No of bytes to be copied
+ * Return: pointer to src
  */
-void 
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
+char *first = dest;
+unsigned int i = 0;
 
-return ;
+while (i < n)
+{
+*dest = *src;
+dest++;
+src++;
+i++;
+}
+
+return (first);
 }
