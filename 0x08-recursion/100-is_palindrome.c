d590fd4f13439248ef68d421949f5be3d56b53c7
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-25 12:39:25 -0700

    task 7 unfinished

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * is_palindrome -checks if string is a palindrome
+ * the strings can be considered identical, otherwise return 0.
+ * @s : pointer to the string
+ *
+ * Return: 1 or 0;
+ */
+int is_palindrome(char *s)
+{
+return (0);
+}
