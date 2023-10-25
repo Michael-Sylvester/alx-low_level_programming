c89c20d9981cd4ae4d489581fe442e528e8dc317
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-25 08:33:52 -0700

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,12 +1,19 @@
 #include "main.h"
 /**
- *  -
- * no parameters
- *
- * Return: nothing
+ * _pow_recursion - a function that x to the power of y
+ * @x :base number
+ * @y : exponential
+ * Return: the result of x to the power y
  */
-void
+int _pow_recursion(int x, int y)
 {
+int pow = x;
+if (y < 0)
+return (-1);
+else if (y == 0)
+return (1);
+else
+pow *= _pow_recursion(x, y - 1);
 
-return;
+return (pow);
 }
