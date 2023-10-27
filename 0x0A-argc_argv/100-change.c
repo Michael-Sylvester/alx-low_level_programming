f47fcde70242a513b499f19453e1a6780b75fee3
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-27 14:00:32 -0700

    task 5 uncompleated

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - a program that adds positive numbers
+ * @argc : the number of arguments
+ * @argv : the pointer to the array of arguments
+ *
+ * Return: nothing
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+if (argc != 2)
+{
+puts("Error");
+return (1);
+}
+
+puts("0");
+return (0);
+}
