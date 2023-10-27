6321f4e395c60d5b0d85569b9a422fe5722a4352
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-27 11:29:55 -0700

    initilaizing files

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - 
+ * @argc : the number of arguments
+ * @argv : the pointer to the array of arguments
+ *
+ * Return: nothing
+ */
+int main(int argc, char *argv[])
+{
+
+return (0);
+}
