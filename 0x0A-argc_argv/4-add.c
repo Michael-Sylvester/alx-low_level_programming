b705cbf5c3c67f8f2d31673ed27ffaae1d0474e6
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-10-27 13:48:20 -0700

    revert to old work

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
 int sum;
 int i;
-int x;
+
 if (argc == 1)
 puts("0");
 else
@@ -22,14 +22,6 @@ for (i = 1; i < argc; i++)
 {
 if (atoi(argv[i]) && atoi(argv[i]) >= 0)
 {
-for (x = 0; x < (int)strlen(argv[i]); x++)
-{
-if (!isdigit(argv[i][x]))
-{
-puts("Error");
-return (1);
-}
-}
 sum += atoi(argv[i]);
 }
 else
