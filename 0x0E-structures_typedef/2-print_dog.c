a37ee86a8d9da12f0ce0392ff671d93a906b4174
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-06 14:25:01 -0800

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "dog.h"
-
+char *check(char *word);
 /**
 * print_dog - a function that prints struct dog
 * @d: pointer to the struct instance
@@ -10,8 +11,23 @@
 void print_dog(struct dog *d)
 {
 
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
-printf("Owner: %s/n", d->owner);
+printf("Name: %s\n", check(d->name));
 
+printf("Age: %f\n", d->age);
+
+printf("Owner: %s\n", check(d->owner));
+
+}
+
+/**
+* check - a function to check if a string variable is NULL
+* @word: the string being checked
+* Return: the apropriate word
+*/
+char *check(char *word)
+{
+if (word != NULL)
+return (word);
+else
+return ("nil");
 }
