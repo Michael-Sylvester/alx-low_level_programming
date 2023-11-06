469de01962725ec7f0f08744e05ef5c713f1b96b
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-06 15:11:35 -0800

    task 4 update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
-* new_dog : function creates a new dog with typdef dog_t
+* new_dog - function creates a new dog with typdef dog_t
 * @name: 1st member
 * @age: 2nd member
 * @owner: 3rd member
@@ -9,7 +9,7 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t * ndog;
+dog_t *ndog;
 ndog = malloc(sizeof(struct dog));
 if (ndog == NULL)
 return (NULL);
