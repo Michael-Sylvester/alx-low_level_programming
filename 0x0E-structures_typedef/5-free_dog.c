18e4a0c9d0a192a07610603573b768dfa8697063
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-06 15:37:17 -0800

    task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,12 +1,15 @@
-#include "main.h"
+#include "dog.h"
+#include <stdlib.h>
 /**
- * 
- * @
- * @
- * Return: nothing
- */
-void
+* free_dog - free all memoery to dog struct
+* @d : pointer to dog;
+* @
+* Return: nothing
+*/
+void free_dog(dog_t *d)
 {
+free(d->name);
+free(d->owner);
+free(d);
 
-return ;
 }
