46866e42022cd06bbd4d877ea30cc2a13418ecb3
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-06 14:46:01 -0800

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,12 +1,17 @@
-#include "main.h"
+#include "dog.h"
+dog_t ndog;
 /**
- * 
- * @
- * @
- * Return: nothing
+ * new_dog : function creates a nw dog with typdef dog_t
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ * Return: pointer to new dog
  */
-void
+dog_t *new_dog(char *name, float age, char *owner)
 {
+  *ndog.name = *name;
+  ndog.age = age;
+  *ndog.owner = *owner;
 
-return ;
+  return (&ndog);
 }
