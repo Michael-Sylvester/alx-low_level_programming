db6b881ad543fd1bcc60d9ee2d0af03d9acf6d12
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-06 13:56:59 -0800

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,12 +1,17 @@
-#include "main.h"
+#include <stdio.h>
+#include "dog.h"
+
 /**
- * 
- * @
- * @
- * Return: nothing
- */
-void
+* print_dog - a function that prints struct dog
+* @d: pointer to the struct instance
+*
+* Return: nothing
+*/
+void print_dog(struct dog *d)
 {
 
-return ;
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+printf("Owner: %s/n", d->owner);
+
 }
