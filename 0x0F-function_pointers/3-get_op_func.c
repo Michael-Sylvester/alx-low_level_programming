33a121ce71a7ca2e832e3985039889eac2c963ca
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-08 15:24:43 -0800

    task 3 test

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <string.h>
 #include <stdlib.h>
 /**
 * get_op_func - sellect the apropriate operation
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,12 +1,34 @@
-#include "function_pointers.h"
+#include <stdlib.h>
+#include "3-calc.h"
+#include <stdio.h>
 /**
- *
- * @
- * @
- * Return: nothing
- */
-void
+* main - the main function for task 3
+* @argc: the number of CMAs
+* @argv: pointer to array of arguments
+*Return: 1
+*/
+int main(int argc, char *argv[])
 {
+int ans;
+int a = atoi(argv[1]);
+int b = atoi(argv[3]);
+char *operator = argv[2];
 
-return;
+if (argc != 4)
+{
+puts("Error");
+ printf("argc = %i\n", argc);
+ exit(98);
+}
+
+if ((*operator == '/' || *operator == '%') && b == 0)
+{
+puts("Error");
+exit(100);
+}
+
+ans = get_op_func(operator)(a, b);
+
+printf("%i\n", ans);
+return (1);
 }
