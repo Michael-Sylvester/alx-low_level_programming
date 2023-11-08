be4665b2b5f3d7f307b9f6914655983b696e74cc
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-08 14:19:42 -0800

    task 3 part 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,12 +1,30 @@
-#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdlib.h>
 /**
- *
- * @
- * @
- * Return: nothing
- */
-void
+* get_op_func - sellect the apropriate operation
+* @s:  string representing function
+* @a: 1st number
+* @b: 2nd number
+* Return: nothing
+*/
+int (*get_op_func(char *s))(int a, int b)
 {
+int i = 0;
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
 
-return;
+while (ops[i].op != NULL)
+{
+if (strcmp(s, ops[i].op) == 0)
+return ops[i].f;
+
+i++;
+}
+return (NULL);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,12 +1,60 @@
 #include "function_pointers.h"
 /**
- *
- * @
- * @
- * Return: nothing
- */
-void
+* op_add - addition
+* @a: 1st number
+* @b: 2nd number
+* Return:sum of a and b
+*/
+int op_add(int a, int b)
 {
 
-return;
+return (a + b);
+}
+
+/**
+* op_sub - subtraction
+* @a: 1st number
+* @b: 2nd number
+* Return: nothing
+*/
+int op_sub(int a, int b)
+{
+
+return (a - b);
+}
+
+/**
+* op_mul - multiplication
+* @a: 1st number
+* @b: 2nd number
+* Return: nothing
+*/
+int op_mul(int a, int b)
+{
+
+return (a * b);
+}
+
+/**
+* op_div - division
+* @a: 1st number
+* @b: 2nd number
+* Return: nothing
+*/
+int op_div(int a, int b)
+{
+
+return (a / b);
+}
+
+/**
+* op_mod - modulo
+* @a: 1st number
+* @b: 2nd number
+* Return: nothing
+*/
+int op_mod(int a, int b)
+{
+
+return (a % b);
 }
