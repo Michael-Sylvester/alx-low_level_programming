39f961179e851fc7578fad37bc22858bc68e543d
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-08 07:26:00 -0800

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,13 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
- *
- * @
- * @
- * Return: nothing
- */
-void
+* print_name - a function that prints a name
+* @name: name to be printed
+* @f: function pointer for printing
+* Return: nothing
+*/
+void print_name(char *name, void (*f)(char *))
 {
+f(name);
 
-return;
 }
