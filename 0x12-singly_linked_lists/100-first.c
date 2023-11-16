80d5ddf8ecc7530545bc33173c0f59b2711e6ea6
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-16 23:55:15 +0000

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ *tortoise - function that called printed before main
+ * @
+ * @
+ * Return: the number of nodes
+ */
+void tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
