40dd14c88945225c2b3f1fe0f4ea56c0ea9707be
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-16 22:54:55 +0000

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,12 +1,33 @@
 #include "lists.h"
 /**
- *
- * @h : pointer to first member of list
- * @
- * Return: nothing
- */
-size_t
+* add_node_end- adds new node at begining of list
+* @head : pointer to first member of list
+* @str: atring to be added to the new head node
+* Return: nothing
+*/
+list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *p;
+	list_t *temp = malloc(sizeof(list_t));
 
-return;
+	if (temp == NULL)
+		return (NULL);
+
+	temp->str = strdup(str);
+	temp->len = strlen(str);
+	temp->next = NULL;
+
+	if (*head == NULL)
+		*head = temp;
+	else
+	{
+		p = *head;
+		while (p->next != NULL)
+		{
+			p = p->next;
+		}
+		p->next = temp;
+	}
+
+	return (temp);
 }
