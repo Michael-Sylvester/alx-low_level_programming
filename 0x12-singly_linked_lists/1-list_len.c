4c68f069efdac2ccc824dbf53478cf95c4558c74
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-16 21:05:28 +0000

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,12 +1,21 @@
 #include "lists.h"
 /**
- *
+ *list_len - count the number of nodes in list
  * @h : pointer to first member of list
  * @
- * Return: nothing
+ * Return: number of nodes
  */
-size_t
+size_t list_len(const list_t *h)
 {
+	unsigned long count = 0;
+	list_t *p = (list_t *)h;
 
-return;
+	while (p != NULL)
+	{
+		count++;
+		p = p->next;
+	}
+
+	return (count);
+
 }
