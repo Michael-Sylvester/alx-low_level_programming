44ce7c379614b99576df34bb6f3f5e3c29b2efcb
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-21 05:41:47 -0800

    task 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * reverse_listint - Coutns number of nodes
+ * @head: pointer to 1st node
+ * @
+ * Return: number of nodes
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *p = *head;
+	listint_t *mynext = p->next;
+
+	if (*head == NULL)
+		return (NULL);
+	p->next = NULL;
+	while (mynext != NULL)
+	{
+		*head = mynext;
+		mynext = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+	}
+
+
+	return (*head);
+}
