ed7f22f1a037d0944788ecd4fce8db9549fa8cf2
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-21 03:46:38 -0800

    task 7 update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	while (count < index && p!= NULL)
+	while (count < index && p != NULL)
 	{
 		count++;
 		p = p->next;
