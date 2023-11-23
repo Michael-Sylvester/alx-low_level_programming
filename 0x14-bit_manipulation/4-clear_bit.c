e32ac9faa5f3b93ef6a83298ac0b281f0e302188
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-23 21:21:09 +0000

    task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,20 @@
 #include "main.h"
 /**
- *
- * @
- * @
- * Return: nothing
+ * clear_bit - set the bit at the index to 0
+ * @n: number to be bit manipulated
+ * @index: the index at which the clear should happen
+ * Return: 1 if good, -1 if bad
  */
-int
+int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned int size = sizeof(unsigned long int) * 8 - 1;
+	unsigned int pos = 1U << index;
 
-return;
+	if (index > size)
+		return (-1);
+
+	if (((*n >> index) & 1) == 1)
+		*n &= ~pos;
+
+	return (1);
 }
