0abab675a38a4a9a86b016ea94ae44c26ccd5200
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-23 20:30:08 +0000

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,12 +1,19 @@
 #include "main.h"
 /**
- *
- * @
- * @
- * Return: nothing
+ * get_bit - finds the value of a bit at a particular index
+ * @n: the number that is being checked
+ * @index: the index of the bit we are looking for
+ * Return: value of the bit
  */
-int
+int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int size = sizeof(unsigned long int) * 8;
+	/*unsigned long int pos = 1UL << (size - 1);
+	unsigned int count = 0;
+	*/
+	if (index > size)
+		return (-1);
+	n >>= index;
 
-return;
+	return (n & 1);
 }
