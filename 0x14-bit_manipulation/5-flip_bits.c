55acbb6ca746b02e2f6c3efe8172f56f37dfe2fc
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-23 23:02:18 +0000

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,12 +1,40 @@
 #include "main.h"
 /**
- *
- * @
- * @
- * Return: nothing
+ * flip_bits - count the number of bits that need to be
+ * flipped in order for the numbers to be equal
+ * @n: the 1st number
+ * @m: the 2nd number
+ * Return: number of flip bits
  */
-int
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned int size = sizeof(unsigned long int) * 8 - 1;
+	unsigned int pos = 0U;
+	int count = 0;
 
-return;
+	while (pos <= size)
+	{
+		if (get_bit(n, pos) ^ get_bit(m, pos))
+			count++;
+		pos++;
+	}
+
+	return (count);
+}
+
+/**
+ * get_bit - finds the value of a bit at a particular index
+ * @n: the number that is being checked
+ * @index: the index of the bit we are looking for
+ * Return: value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int size = sizeof(unsigned long int) * 8;
+
+	if (index > size)
+		return (-1);
+	n >>= index;
+
+	return (n & 1);
 }
