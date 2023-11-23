f5dc93ce717690c61018b611fbc8926589a1837f
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-23 19:44:27 +0000

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,12 +1,33 @@
 #include "main.h"
 /**
- *
- * @
+ * print_binary - change decimals into binary and print
+ * @n: the number to be changed
  * @
  * Return: nothing
  */
-int
+void print_binary(unsigned long int n)
 {
+	int size = sizeof(unsigned long int) * 8 - 1;
+	unsigned long int pos = 1UL << size;
+	int start = 0;
 
-return;
+	printf("size = %d and pos = %lu\n", size, pos);
+	while (size >= 0)
+	{
+		if ((n & pos) != 0)
+		{
+			_putchar('1');
+			start = 1;
+		}
+		else if (start)
+			_putchar('0');
+		size--;
+		pos >>= 1;
+	}
+
+	if (!start)
+	{
+		printf("size = %d and pos = %lu\n", size, pos);
+		_putchar('0');
+	}
 }
