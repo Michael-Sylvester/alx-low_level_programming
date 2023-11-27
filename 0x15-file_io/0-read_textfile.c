1340238fc400c3dd0bf168887cd8df58021be4d1
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-27 18:51:05 +0000

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,12 +1,50 @@
 #include "main.h"
 /**
- *
- * @
- * @
+ * read_textfile - read a tect fileand print it's contetnts
+ * @filename: pointer to the file descriptor ?
+ * @letters: number of letters expected
  * Return: nothing
  */
-int
+ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buff = malloc(letters);
+	int filed = open(filename, O_RDONLY);
+	int readf;
+	ssize_t count;
 
-return;
+	if (filename == NULL)
+		return (0);
+	if (filed == -1)
+			return (0);
+
+	readf = read(filed, buff, letters);
+
+	if (readf == -1 || readf == 0)
+	{
+		free(buff);
+		close(filed);
+		return (0);
+	}
+	
+	count = write(STDOUT_FILENO, buff, readf);
+
+	free(buff);
+	close(filed);
+	return (count);
+}
+
+int main(int ac, char **av)
+{
+	ssize_t n;
+
+	if (ac != 2)
+		{
+			dprintf(2, "Usage: %s filename\n", av[0]);
+			exit(1);
+		}
+	n = read_textfile(av[1], 114);
+	printf("\n(printed chars: %li)\n", n);
+	n = read_textfile(av[1], 1024);
+	printf("\n(printed chars: %li)\n", n);
+	return (0);
 }
