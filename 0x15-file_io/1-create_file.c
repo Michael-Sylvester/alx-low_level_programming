a453e75a3a7ab88fdb5dbf59d2a3b94826d0db55
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-27 19:58:45 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,26 @@
 #include "main.h"
 /**
- *
- * @
- * @
- * Return: nothing
+ *create_file - create a file and fill it with text
+ * @filename: tring with the ame of the file
+ * @text_content: the string to be placed in the file
+ * Return: 1 on success, -1 on fail
  */
-int
+int create_file(const char *filename, char *text_content)
 {
-
-return;
+	int filed = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600 );
+	int writef;
+	if (filename == NULL || filed == -1)
+		return (-1);
+	if (text_content == NULL)
+		writef = write(filed, "", strlen(text_content));
+	else
+		writef = write(filed, text_content, strlen(text_content));
+	if (writef == -1)
+	{
+		close(filed);
+		return(-1);
+	}
+	
+	close(filed);
+	return (1);
 }
