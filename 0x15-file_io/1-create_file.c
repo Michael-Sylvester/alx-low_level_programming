1f7a486f5939621cb98c2daaac1b6ed8a19e3bfd
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-27 20:04:17 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,20 +7,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int filed = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600 );
+	int filed = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	int writef;
+
 	if (filename == NULL || filed == -1)
 		return (-1);
+	
 	if (text_content == NULL)
-		writef = write(filed, "", strlen(text_content));
+		writef = write(filed, "", 0);
 	else
 		writef = write(filed, text_content, strlen(text_content));
+
 	if (writef == -1)
 	{
 		close(filed);
-		return(-1);
+		return (-1);
 	}
-	
+
 	close(filed);
 	return (1);
 }
