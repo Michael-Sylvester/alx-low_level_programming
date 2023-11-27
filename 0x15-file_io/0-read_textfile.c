24720a88f14e255e0884f2efacd51f62a174d821
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-27 18:55:02 +0000

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	if (filed == -1)
-			return (0);
+		return (0);
 
 	readf = read(filed, buff, letters);
 
@@ -25,26 +25,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(filed);
 		return (0);
 	}
-	
+
 	count = write(STDOUT_FILENO, buff, readf);
 
 	free(buff);
 	close(filed);
 	return (count);
-}
-
-int main(int ac, char **av)
-{
-	ssize_t n;
-
-	if (ac != 2)
-		{
-			dprintf(2, "Usage: %s filename\n", av[0]);
-			exit(1);
-		}
-	n = read_textfile(av[1], 114);
-	printf("\n(printed chars: %li)\n", n);
-	n = read_textfile(av[1], 1024);
-	printf("\n(printed chars: %li)\n", n);
-	return (0);
 }
