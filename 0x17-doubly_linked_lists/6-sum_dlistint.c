0d3fc2e523ff0d1cf7fe459496b01d56030e98f3
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-11 17:06:54 +0000

    task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,12 +1,27 @@
 #include "lists.h"
 /**
- *
+ * sum_dlistint - add sum of all data -> n in nodes
+ * @head: pointer to head of node
  * @
- * @
- * Return: nothing
+ * Return: sum of n or 0 if empty
  */
-
+int sum_dlistint(dlistint_t *head)
 {
+	dlistint_t *temp = head;
+	int sum = 0;
 
-return;
+	if (head == NULL)
+		return (0);
+
+	sum += head->n;
+	temp = head->next;
+
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+
+	return (sum);
 }
