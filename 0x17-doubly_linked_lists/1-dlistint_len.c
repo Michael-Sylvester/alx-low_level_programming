f094b2d9e47fc4a8c160364728ad0d141d1016b3
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-11 12:51:17 +0000

    task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,27 @@
 #include "lists.h"
 /**
- *
+ *dlistint_len - counts number of elemnts in doubly linked list
+ * @h: head of list
  * @
- * @
- * Return: nothing
+ * Return: number of elements in list
  */
-
+size_t dlistint_len(const dlistint_t *h)
 {
+	dlistint_t *temp = (dlistint_t *)h;
+	int count = 0;
 
-return;
+	if (h == NULL)
+		return (0);
+
+	count++;
+	temp = h->next;
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+
+
+	return (count);
 }
