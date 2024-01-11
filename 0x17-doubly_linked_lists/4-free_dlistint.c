370dd0155f95f8da3ea903ebac2b8b0fdbc83af7
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-11 16:41:45 +0000

    task 4 head is NULL check

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,8 +8,10 @@
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp = head;
-	dlistint_t *next = temp->next;
+	dlistint_t *next = NULL;
 
+	if (temp != NULL)
+		next = temp->next;
 	while (next != NULL)
 	{
 		free(temp);
