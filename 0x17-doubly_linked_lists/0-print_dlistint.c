dc0917fda8e65c123e8cb5a79ff890c13dff67a4
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-11 12:44:28 +0000

    betty check

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,30 +1,29 @@
 #include "lists.h"
 /**
- *size_t print_dlistint - prints all the elements of a list
+ * print_dlistint - prints all the elements of a list
  * @h : the head of the list
- * 
+ *
  * Return: nothing
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	dlistint_t *temp = (dlistint_t *)h;
 	int count = 0;
-	
+
 	if (h == NULL)
 		return (0);
-	else
-	{
-		printf("%d\n", h->n);
-		count++;
-		temp = h->next;
-	}
+
+	printf("%d\n", h->n);
+	count++;
+	temp = h->next;
+
 	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
 		temp = temp->next;
 		count++;
 	}
-	
 
-	return count;
+
+	return (count);
 }
