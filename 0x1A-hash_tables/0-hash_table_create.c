e01799ea44161d87db8270460f4d9168aa4dfae5
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-02 15:00:54 +0000

    task 0: update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size > 0)
 	{
-		table->array = malloc(size * sizeof(hash_node_t));
+		table->array = malloc(size * sizeof(hash_node_t *));
 		if (table->array == NULL || size == 0)
 		{
 			free(table);
