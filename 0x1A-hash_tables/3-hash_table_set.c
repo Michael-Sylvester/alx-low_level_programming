06fa740abc880e1951d769367b4ef0cc6607abb5
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-02 18:37:10 +0000

    task 3 update: incorrect malloc for value and key fixed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,11 +11,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 	int status = 0;
 
-	if (strcmp(key, "") == 0 || key == NULL)
+	if (key == NULL || strcmp(key, "") == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 
-	status = insert(ht->array[index], value, key);
+	status = insert(&ht->array[index], value, key);
 	return (status);
 }
 
@@ -26,53 +26,26 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  *@key: the name of the hash key
  *Return: 1 if succesfull otherwise 0
  */
-int insert(hash_node_t *head, const char *value, const char *key)
+int insert(hash_node_t **head, const char *value, const char *key)
 {
 	hash_node_t *temp = malloc(sizeof(hash_node_t));
 
 	if (temp == NULL)
 		return (0);
 
-	strcpy(temp->key, (char *)key);
-	strcpy(temp->value, (char *)value);
-
-	temp->next = head;
-	head = temp;
-
-	return (1);
-}
-
-/**
- *insert - inserts a new value at the end of the linked list
- *@head: the head of the array
- *@value: the data to go in the list
- *@key: the name of the hash key
- *Return: 1 if succesfull otherwise 0
- */
-int insertend(hash_node_t *head, const char *value, const char *key)
-{
-	hash_node_t *p;
-	hash_node_t *temp = malloc(sizeof(hash_node_t));
-
-	if (temp == NULL)
+	temp->key = malloc(strlen(key) + 1);
+	temp->value = malloc(strlen(value) + 1);
+	if (temp->key == NULL || temp->value == NULL)
+	{
+		free(temp);
 		return (0);
+	}
 
 	strcpy(temp->key, (char *)key);
 	strcpy(temp->value, (char *)value);
-	temp->next = NULL;
 
-	if (head == NULL)
-		head = temp;
-	else
-		{
-			p = head;
-			while (p->next != NULL)
-				{
-					p = p->next;
-				}
-			p->next = temp;
-		}
+	temp->next = *head;
+	*head = temp;
 
 	return (1);
-
 }
