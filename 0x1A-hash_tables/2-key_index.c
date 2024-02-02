1ded7945d5911f8440957e2c8cd8b2eed64ad2a0
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-02 16:08:38 +0000

    task 2: find index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+*key_index - calculates the index for the given has table
+*@key: the string key assigned to the data
+*@size: the size of the hash table array
+*Return: index of the key
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index = 0;
+	unsigned long int hash = 0;
+	
+	hash = hash_djb2(key);
+	index = hash % size;
+
+	return (index);
+}
