428911ec51f135eb36a569e0af6ee321baa818c8
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-02 14:24:57 +0000

    task 0: update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,32 +1,33 @@
 #include "hash_tables.h"
 /**
- * hash_table_create - Function to create a new hash table
- * @size: the size of the hash table array
- * Return: return the new hash table
+* hash_table_create - Function to create a new hash table
+* @size: the size of the hash table array
+* Return: return the new hash table
 */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    unsigned int index = 0;
-    hash_table_t *table = malloc(sizeof(hash_table_t));
-    
-    
-    table->array = malloc(size * sizeof(hash_node_t));
-    if (table->array == NULL)
-        return (NULL);
-   
-    table->size = size;
+	unsigned int index = 0;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 
-    while (index < size)
-    {
-        table->array[index] = NULL;
-        index++;
-    }
+	if (table == NULL)
+	{
+		return (NULL);
+	}
 
-    return (table);
+	table->array = malloc(size * sizeof(hash_node_t));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	table->size = size;
+
+	while (index < size)
+	{
+		table->array[index] = NULL;
+		index++;
+	}
+
+	return (table);
 }
-/*Prototype: hash_table_t *hash_table_create(unsigned long int size);
-where size is the size of the array
-Returns a pointer to the newly created hash table
-If something went wrong, your function should return NULL*/
-
