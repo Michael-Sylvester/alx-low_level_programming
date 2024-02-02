81f1ebcaa0c2e9fce9613debc83b0a7fb5c57ffd
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-02 18:48:46 +0000

    checkif argumenyts are null

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 	int status = 0;
 
-	if (key == NULL || strcmp(key, "") == 0)
+	if (key == NULL || value == NULL || ht == NULL || strcmp(key, "") == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 
@@ -34,6 +34,7 @@ int insert(hash_node_t **head, const char *value, const char *key)
 		return (0);
 
 	temp->key = malloc(strlen(key) + 1);
+	
 	temp->value = malloc(strlen(value) + 1);
 	if (temp->key == NULL || temp->value == NULL)
 	{
