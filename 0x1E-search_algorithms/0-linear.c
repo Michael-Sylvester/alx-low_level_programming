737388f2a11c354f09027d060946caabc5f88d8b
Michael Sylvester <42616131+Michael-Sylvester@users.noreply.github.com>
2024-05-08 19:54:24 +0000

    task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,18 +12,13 @@ int linear_search(int *array, size_t size, int value)
 {
 	int index = 0;
 
-	if (array ==  NULL || size < 1)
+	if (array ==  NULL)
 		return (-1);
 	for (; index < (int)size; index++)
 	{
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
 		if (array[index] == value)
-		{
 			return (index);
-		}
-		else
-		{
-			printf("Value checked array[%d] = [%d]\n", index, array[index]);
-		}
 	}
 
 	return (-1);
